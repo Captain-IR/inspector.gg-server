f32a09e691cf2967ec3eb2295e5972d748f9aabe
Captain IR <52345388+Captain-IR@users.noreply.github.com>
2020-10-03 18:39:46 +0200

    Added dynamic region

--- routes.js
+++ routes.js
@@ -17,8 +17,11 @@ const api = new LolApi({
 
 router.get('/summoner', async function (req, res, next) {
 	const summonerName = req.query.name
+	const region = req.query.region
+	console.log('Summoner Name: ', summonerName)
+	console.log('Region: ', region)
 	try {
-		const summoner = await api.Summoner.getByName(summonerName, Constants.Regions.EU_WEST)
+		const summoner = await api.Summoner.getByName(summonerName, Constants.Regions[region])
 		res.status(200).json({ summoner: summoner.response })
 	} catch (error) {
 		console.log(error)
@@ -27,13 +30,14 @@ router.get('/summoner', async function (req, res, next) {
 
 router.get('/matches', async function (req, res, next) {
 	const summonerId = req.query.summonerId
+	const region = req.query.region
 	try {
-		const data = await api.Match.list(summonerId, Constants.Regions.EU_WEST)
+		const data = await api.Match.list(summonerId, Constants.Regions[region])
 		const first5matches = data.response.matches.slice(1, 6)
 		let minifiedArray = []
 		minifiedArray = first5matches.map(async match => {
 			const championName = await getChampionName(match.champion)
-			const game = await getMatchData(match.gameId)
+			const game = await getMatchData(match.gameId, region)
 			return {
 				...match,
 				game: game, // match populate
@@ -47,71 +51,6 @@ router.get('/matches', async function (req, res, next) {
 	}
 })
 
-// router.get('/match', async function (req, res, next) {
-// 	const matchId = req.query.id
-// 	try {
-// 		// const match = await api.Match.get(matchId, Constants.Regions.EU_WEST)
-// 		// const teams = match.response.teams.map(team => ({
-// 		// 	teamId: team.teamId,
-// 		// 	win: team.win,
-// 		// }))
-// 		// const participants = match.response.participants.map(async part => {
-// 		// 	const champion = await getChampionName(part.championId)
-// 		// 	return {
-// 		// 		participantId: part.participantId,
-// 		// 		teamId: part.teamId,
-// 		// 		champion,
-// 		// 	}
-// 		// })
-// 		// const parts = await Promise.all(participants)
-
-// 		// const participantIdentities = match.response.participantIdentities.map(partId => ({
-// 		// 	participantId: partId.participantId,
-// 		// 	player: partId.player.summonerName,
-// 		// 	profileIcon: partId.player.profileIcon,
-// 		// }))
-
-// 		// let newParts = []
-// 		// for (const part of parts) {
-// 		// 	for (const partId of participantIdentities) {
-// 		// 		if (part.participantId === partId.participantId) {
-// 		// 			// console.log('Champion ID', part.championId)
-// 		// 			newParts.push({
-// 		// 				...part,
-// 		// 				player: partId.player,
-// 		// 				profileIcon: partId.profileIcon,
-// 		// 			})
-// 		// 		}
-// 		// 	}
-// 		// }
-// 		// let total = []
-// 		// for (const team of teams) {
-// 		// 	for (const part of newParts) {
-// 		// 		if (team.teamId === part.teamId) {
-// 		// 			total.push({ ...team, ...part })
-// 		// 		}
-// 		// 	}
-// 		// }
-// 		// console.log('newParts: ', newParts)
-// 		// console.log('total: ', total)
-// 		// console.log('teams: ', teams)
-// 		// console.log('participants: ', participants)
-// 		// console.log('participantIdentities: ', participantIdentities)
-// 		// res.status(200).json({
-// 		// 	match: {
-// 		// 		gameMode: match.response.gameMode,
-// 		// 		total,
-// 		// 	},
-// 		// })
-// 		const total = await getMatchData(matchId)
-// 		res.status(200).json({
-// 			total,
-// 		})
-// 	} catch (error) {
-// 		console.log(error)
-// 	}
-// })
-
 router.get('/champion', async function (req, res, next) {
 	const championId = req.query.id
 	getChampionName(championId).then(champ => {
@@ -119,8 +58,8 @@ router.get('/champion', async function (req, res, next) {
 	})
 })
 
-async function getMatchData(gameId) {
-	const match = await api.Match.get(gameId, Constants.Regions.EU_WEST)
+async function getMatchData(gameId, region) {
+	const match = await api.Match.get(gameId, Constants.Regions[region])
 	let newParticipants = []
 	for (const part of match.response.participants) {
 		for (const partId of match.response.participantIdentities) {
@@ -149,11 +88,8 @@ async function getChampionName(championId) {
 		const champions = await api.DataDragon.getChampion()
 		const championNames = Object.keys(champions.data)
 		const champion = championNames.find(champ => {
-			// console.log('champion key: ', champions.data[champ].key)
-			// console.log('champion id: ', championId)
 			return champions.data[champ].key == championId
 		})
-		// console.log(champion)
 		return champions.data[champion].id // championName
 	} catch (error) {
 		console.log(error)
@@ -161,3 +97,68 @@ async function getChampionName(championId) {
 }
 
 module.exports = router
+
+// router.get('/match', async function (req, res, next) {
+// 	const matchId = req.query.id
+// 	try {
+// 		const match = await api.Match.get(matchId, Constants.Regions.EU_WEST)
+// 		const teams = match.response.teams.map(team => ({
+// 			teamId: team.teamId,
+// 			win: team.win,
+// 		}))
+// 		const participants = match.response.participants.map(async part => {
+// 			const champion = await getChampionName(part.championId)
+// 			return {
+// 				participantId: part.participantId,
+// 				teamId: part.teamId,
+// 				champion,
+// 			}
+// 		})
+// 		const parts = await Promise.all(participants)
+
+// 		const participantIdentities = match.response.participantIdentities.map(partId => ({
+// 			participantId: partId.participantId,
+// 			player: partId.player.summonerName,
+// 			profileIcon: partId.player.profileIcon,
+// 		}))
+
+// 		let newParts = []
+// 		for (const part of parts) {
+// 			for (const partId of participantIdentities) {
+// 				if (part.participantId === partId.participantId) {
+// 					// console.log('Champion ID', part.championId)
+// 					newParts.push({
+// 						...part,
+// 						player: partId.player,
+// 						profileIcon: partId.profileIcon,
+// 					})
+// 				}
+// 			}
+// 		}
+// 		let total = []
+// 		for (const team of teams) {
+// 			for (const part of newParts) {
+// 				if (team.teamId === part.teamId) {
+// 					total.push({ ...team, ...part })
+// 				}
+// 			}
+// 		}
+// 		console.log('newParts: ', newParts)
+// 		console.log('total: ', total)
+// 		console.log('teams: ', teams)
+// 		console.log('participants: ', participants)
+// 		console.log('participantIdentities: ', participantIdentities)
+// 		res.status(200).json({
+// 			match: {
+// 				gameMode: match.response.gameMode,
+// 				total,
+// 			},
+// 		})
+// 		const total = await getMatchData(matchId)
+// 		res.status(200).json({
+// 			total,
+// 		})
+// 	} catch (error) {
+// 		console.log(error)
+// 	}
+// })
